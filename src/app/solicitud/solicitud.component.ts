3432162176beea4dff60969870558c2e0dbe9d07
alacrema <alacrema@jcyl.es>
2022-04-27 23:31:06 +0200

    Ejercicio 2

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudComponent implements OnInit {
 
+  solicitud = {nombre: "Marta"};
+
   constructor() { }
 
   ngOnInit(): void {
